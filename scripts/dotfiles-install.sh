729a1610e6811862344b32c6d9d4ca093949461b
Mr-peipei <wushumenmenmen2@gmail.com>
2022-10-10 20:02:58 +0900

    fix vscode install shell

--- scripts/dotfiles-install.sh
+++ scripts/dotfiles-install.sh
@@ -5,16 +5,15 @@
 
 function main () {
   local CURRENT=$(cd $(dirname $0);pwd)
-  echo $CURRENT
 
   # sudo apt update
   # sudo apt upgrade
 
   echo "Welcome to Mr-peipei dotfiles"
-  # source $current_dir/lib/install-curl.sh
-  # source $current_dir/lib/install-neovim.sh
-  # source $current_dir/lib/install-zsh.sh
-  # source $current_dir/lib/install-vscode.sh
+  # source $CURRENT/lib/install-curl.sh
+  # source $CURRENT/lib/install-neovim.sh
+  # source $CURRENT/lib/install-zsh.sh
+  # source $CURRENT/scripts/lib/install-vscode.sh $CURRENT
 }
 
 main
--- scripts/lib/install-vscode.sh
+++ scripts/lib/install-vscode.sh
@@ -3,23 +3,28 @@
 #          VSCode                                         ##
 #--------------------------------------------------------------#
 
-local CURRENT=$(cd $(dirname $0);pwd)
-echo $CURRENT
+set -ue
 
 function vscode() {
-  echo "start installing vscode"
+  echo $1
+  echo "Start installing vscode"
 
-  curl -L https://go.microsoft.com/fwlink/?LinkID=760868 -o vscode.deb
+  curl -L 'https://go.microsoft.com/fwlink/?LinkID=760868' -o vscode.deb
   sudo apt install ./vscode.deb
+  rm -f ./vscode.deb
 
   echo "Successfully download vscode"
 
   echo "Set up a vscode dotfiles"
-  cp $CURRENT/vscode/settings.json ~/.config/Code/User/
-  cp $CURRENT/vscode/keybindings.json ~/.config/Code/User/
-  cp $CURRENT/vscode/snippets ~/.config/Code/User/
-  cp -r $CURRENT/vscode/.vscode ~/.
+
+  mkdir -p ~/.config/Code/User/
+  cp $1/vscode/settings.json ~/.config/Code/User/
+  cp $1/vscode/keybindings.json ~/.config/Code/User/
+  cp -r $1/vscode/snippets ~/.config/Code/User/
+  cp -r $1/vscode/.vscode ~/.
+
+  echo "Successfully set up a vscode dotfiles"
 }
 
 
-vsocde
+vscode $1
