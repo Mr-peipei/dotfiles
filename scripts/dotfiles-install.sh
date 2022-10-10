99bba0e39237570219bfb7e70e299a8705cf4b2c
Mr-peipei <wushumenmenmen2@gmail.com>
2022-10-11 00:21:18 +0900

    fix indent

--- scripts/dotfiles-install.sh
+++ scripts/dotfiles-install.sh
@@ -4,17 +4,19 @@
 #--------------------------------------------------------------#
 
 function main () {
-  local CURRENT=$(cd $(dirname $0);pwd)
+  echo "Welcome to Mr-peipei dotfiles"
+
+  # local CURRENT=$(cd $(dirname $0);pwd)
 
   # echo $1 | sudo -s apt update
   # echo $1 | sudo -s apt upgrade
 
-  echo "Welcome to Mr-peipei dotfiles"
+
   # source $CURRENT/lib/install-curl.sh
   # source $CURRENT/lib/install-neovim.sh
   # source $CURRENT/lib/install-zsh.sh
   # source $CURRENT/lib/install-wezterm.sh $1
-  source $CURRENT/scripts/lib/install-vscode.sh $1 $CURRENT
+  # source $CURRENT/scripts/lib/install-vscode.sh $1 $CURRENT
 }
 
 main $1
