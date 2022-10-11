b87da4bf548526ce2cc312dd58094a784c727a04
Mr-peipei <wushumenmenmen2@gmail.com>
2022-10-11 23:12:08 +0900

    change neovim install scripts

--- scripts/lib/install-neovim.sh
+++ scripts/lib/install-neovim.sh
@@ -3,9 +3,22 @@
 #          Neovim                                             ##
 #--------------------------------------------------------------#
 
+set -ue
+
 function neovim() {
-  # for ubuntu
-  sudo apt install neovim -y
+  echo "Start installing neovim"
+
+  echo $1 | sudo -s apt install neovim -y
+
+  echo "Successfully install vscode"
+
+  echo "Set up a neovim dotfiles"
+	mkdir -p ~/.config/nvim/
+  cp -r $2/neovim/* ~/.config/nvim/
+
+	#Install Packer
+	git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+	~/.local/share/nvim/site/pack/packer/start/packer.nvim
 }
 
-neovim
+neovim $1 $2
