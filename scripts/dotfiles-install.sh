77d0f2c2bf7acd850580e44f3e87077d2fa125a4
Mr-peipei <wushumenmenmen2@gmail.com>
2022-10-11 23:25:49 +0900

    fix path

--- scripts/dotfiles-install.sh
+++ scripts/dotfiles-install.sh
@@ -12,11 +12,11 @@ function main () {
   # echo $1 | sudo -s apt upgrade
 
 
-  # source $CURRENT/scripts/lib/install-curl.sh
-  source $CURRENT/scripts/lib/install-neovim.sh $1 $CURRENT
-  # source $CURRENT/scripts/lib/install-zsh.sh
-  # source $CURRENT/scripts/lib/install-wezterm.sh $1
-  # source $CURRENT/scripts/lib/install-vscode.sh $1 $CURRENT
+  # source $CURRENT/lib/install-curl.sh
+  source $CURRENT/lib/install-neovim.sh $1 $CURRENT
+  # source $CURRENT/lib/install-zsh.sh
+  # source $CURRENT/lib/install-wezterm.sh $1
+  # source $CURRENT/lib/install-vscode.sh $1 $CURRENT
 }
 
 main $1
