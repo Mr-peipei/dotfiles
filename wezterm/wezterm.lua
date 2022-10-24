dcf01da5fda112882b3d75c6b3fce16beb7c6dd3
Mr-peipei <wushumenmenmen2@gmail.com>
2022-10-24 23:19:25 +0900

    add wezterm settings

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,19 @@
+
+
+return {
+  color_schme = 'Batman',
+  window_background_opacity = 0.9,
+  window_background_image = '/home/murakami/.config/wezterm/cat.jpg',
+
+  window_background_image_hsb = {
+    -- Darken the background image by reducing it to 1/3rd
+    brightness = 0.3,
+
+    -- You can adjust the hue by scaling its value.
+    -- a multiplier of 1.0 leaves the value unchanged.
+    hue = 1.0,
+
+    -- You can adjust the saturation also.
+    saturation = 1.0,
+  },
+}
